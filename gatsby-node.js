e9e0e5ababd7c4ce8671218681d5dcb64ab99a21
Juliet <rubin.juliet@gmail.com>
2020-09-03 16:09:39 -0400

    added the store

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,6 +14,13 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
         }
+        allDatoCmsStore {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
       }
     `).then(result => {
       result.data.allDatoCmsWork.edges.map(({ node: work }) => {
@@ -22,6 +29,15 @@ exports.createPages = ({ graphql, actions }) => {
           component: path.resolve(`./src/templates/work.js`),
           context: {
             slug: work.slug,
+          },
+        })
+      })
+      result.data.allDatoCmsStore.edges.map(({ node: store }) => {
+        createPage({
+          path: `store/${store.slug}`,
+          component: path.resolve(`./src/templates/store.js`),
+          context: {
+            slug: store.slug,
           },
         })
       })
--- src/components/layout.js
+++ src/components/layout.js
@@ -77,6 +77,9 @@ const TemplateWrapper = ({ children }) => {
                   <Link to="/">Home</Link>
                 </li>
                 <li>
+                  <Link to="/store">Store</Link>
+                </li>
+                <li>
                   <Link to="/about">About</Link>
                 </li>
               </ul>
